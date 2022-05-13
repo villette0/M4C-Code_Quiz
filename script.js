62d4e7200bec453b4419146761b66d756831c677
violet978 <villette@live.com>
2022-05-12 20:40:40 -0700

    Added core functions.

--- script.js
+++ script.js
@@ -8,5 +8,25 @@ var answerList = document.querySelector(".answer-list");
 var nextButton = document.querySelector(".next-button");
 var endContainer = document.querySelector(".end-container");
 
+questionContainer.style.display="none";
 
+startButton.addEventListener("click", function(){
+    questionContainer.style.display="block";
+    startContainer.style.display="none";
+    startGame();
+});
+
+var timeLeft = 10;
+function setTimer() {
+    timeLeft--;
+    timer.textContent=timeLeft;
+    if (timeLeft === 0) {
+        clearInterval(quizTime);
+    }
+}
+
+var quizTime;
+function startGame() {
+    quizTime = setInterval(setTimer, 1000);
+}
 
