8be94e9f56545c78e40371471b33ab7665c6f1a8
violet978 <villette@live.com>
2022-05-12 20:39:54 -0700

    Added universal variables.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // Universal variables first
+var startContainer = document.querySelector(".start-container");
+var timer = document.querySelector(".timer");
+var startButton = document.querySelector(".start-btn");
+var questionContainer= document.querySelector(".question-container");
+var questionText = document.querySelector(".question-text");
+var answerList = document.querySelector(".answer-list");
+var nextButton = document.querySelector(".next-button");
+var endContainer = document.querySelector(".end-container");
+
 
 
